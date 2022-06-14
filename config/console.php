3098171f0e65906a4cb185293e92f129798ac408
user <>
2022-06-13 23:34:30 -0300

    fix yii

--- config/console.php
+++ config/console.php
@@ -2,16 +2,7 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-// LOAD ENV vars in dev
-if(getenv('PROD') == null || getenv('PROD') != 'true'){
-    // comment out the following two lines when deployed to production
-    defined('YII_DEBUG') or define('YII_DEBUG', true);
-    defined('YII_ENV') or define('YII_ENV', 'dev');
-    
-    $dotenv =  \Dotenv\Dotenv::createUnsafeImmutable(__DIR__,'../.env');
-    $dotenv->load();
-    $dotenv->required(['DB_HOST', 'DB_NAME', 'DB_USER', 'DB_PASS']);
-}
+
 
 $params = require __DIR__ . '/params.php';
 $db = require __DIR__ . '/db.php';
